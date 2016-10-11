d768863a9c4c746c1205f2c6e7b961e85e724771
simon <simon.tanner28@gmail.com>
2016-10-11 16:24:04 +0000

    added request-json

---
+++ library/request-json.js
@@ -0,0 +1,19 @@
+var request = require('request');
+
+function requestJson (url, cb) {
+    request(url, function(err, result) {
+        if (err) {
+            cb(err);
+        }
+        else {
+            try {
+                cb(null, JSON.parse(result.body));
+            }
+            catch(err) {
+                cb(err);
+            }
+        }
+    });
+}
+
+exports.requestJson = requestJson;
